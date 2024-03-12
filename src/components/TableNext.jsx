7a9784b1e4e53e3dce5d1dbfebed220d6fa56a10
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 <picard.boris@gmail.com>
2024-03-12 22:58:06 +0100

    ajout de données des données dans la table

--- src/components/TableNext.jsx
+++ src/components/TableNext.jsx
@@ -1,14 +1,22 @@
 import { useEffect, useState } from "react";
+import { Image } from "@nextui-org/image";
 
 export default function TableNext() {
   const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     const fetchAsync = async () => {
-      const response = await fetch(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin%2Cethereum&x_cg_demo_api_key=CG-1t8kdBZJMA1YUmpjF5nypF6R"
-      );
-      const data = await response.json();
-      setPosts(data);
+      try {
+        const response = await fetch(
+          "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&x_cg_demo_api_key=CG-1t8kdBZJMA1YUmpjF5nypF6R"
+        );
+        const data = await response.json();
+        setPosts(data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      }
     };
     fetchAsync();
   }, []);
@@ -18,27 +26,38 @@ export default function TableNext() {
       <table className="table-auto">
         <thead>
           <tr>
-            {posts.map((post, index) => (
-              <th key={index}>{post.name}</th>
-            ))}
+            <th className="font-bold">Name </th>
+            <th className="font-bold">Price </th>
+            <th className="font-bold">Last 24h </th>
+            <th className="font-bold">ATH </th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            {posts.map((post, index) => (
-              <td key={index}>{post.current_price}</td>
-            ))}
-          </tr>
-          <tr>
-            {posts.map((post, index) => (
-              <td key={index}>{post.price_change_24h}</td>
-            ))}
-          </tr>
-          <tr>
-            {posts.map((post, index) => (
-              <td key={index}>ATH : {post.ath}</td>
-            ))}
-          </tr>
+          {loading ? (
+            <p>Loading...</p>
+          ) : (
+            <tr>
+              {posts.map((post, index) => (
+                <tr key={index}>
+                  <td>
+                    {post.market_cap_rank}.
+                    <Image width={25} alt={post.name} src={post.image} />
+                  </td>
+                  <td
+                    key={post.name}
+                    className={`${
+                      post.name === "Chiliz" ? "text-red-500" : ""
+                    }`}
+                  >
+                    {post.name}
+                  </td>
+                  <td key={post.current_price}>{post.current_price}</td>
+                  <td key={post.price_change_24h}>{post.price_change_24h}</td>
+                  <td key={post.ath}>ATH : {post.ath}</td>
+                </tr>
+              ))}
+            </tr>
+          )}
         </tbody>
       </table>
     </>
