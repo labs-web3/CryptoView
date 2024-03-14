dfe62c295117f350c27e5e2cdf5485ae1fd87f15
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 <picard.boris@gmail.com>
2024-03-14 10:15:14 +0100

    ajout de données dans mon tableau

--- src/components/TableNext.jsx
+++ src/components/TableNext.jsx
@@ -31,10 +31,48 @@ export default function TableNext() {
   console.log(posts);
 
   const formatPercentage = (value) => {
-    return Intl.NumberFormat({
+    return Intl.NumberFormat("en-US", {
+      style: "percent",
       minimumFractionDigits: 1,
-      maximumFractionDigits: 2,
-    }).format(value);
+      maximumFractionDigits: 1,
+      signDisplay: "never",
+    }).format(value / 100);
+  };
+
+  const arrowUpOrDown = (value) => {
+    if (value.toString().startsWith("-")) {
+      const arrow = (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          className="w-5 h-5"
+        >
+          <path
+            fillRule="evenodd"
+            d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z"
+            clipRule="evenodd"
+          />
+        </svg>
+      );
+      return arrow;
+    } else {
+      const arrow = (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          className="w-5 h-5"
+        >
+          <path
+            fillRule="evenodd"
+            d="M9.47 6.47a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 1 1-1.06 1.06L10 8.06l-3.72 3.72a.75.75 0 0 1-1.06-1.06l4.25-4.25Z"
+            clipRule="evenodd"
+          />
+        </svg>
+      );
+      return arrow;
+    }
   };
 
   return (
@@ -47,7 +85,6 @@ export default function TableNext() {
             <TableHead className="font-bold">1 h </TableHead>
             <TableHead className="font-bold">24 h </TableHead>
             <TableHead className="font-bold">7 j </TableHead>
-            <TableHead className="font-bold">Last 24h </TableHead>
             <TableHead className="font-bold">ATH </TableHead>
           </TableRow>
         </TableHeader>
@@ -76,33 +113,58 @@ export default function TableNext() {
                 <TableCell key={post.current_price}>
                   ${post.current_price}
                 </TableCell>
-                <TableCell key={post.price_change_percentage_1h_in_currency}>
-                  {formatPercentage(
-                    post.price_change_percentage_1h_in_currency
-                  )}
-                  %
-                </TableCell>
-                <TableCell key={post.price_change_percentage_24h_in_currency}>
-                  {formatPercentage(
-                    post.price_change_percentage_24h_in_currency
-                  )}
-                  %
-                </TableCell>
-                <TableCell key={post.price_change_percentage_7d_in_currency}>
-                  {formatPercentage(
-                    post.price_change_percentage_7d_in_currency
-                  )}
-                  %
-                </TableCell>
                 <TableCell
-                  key={post.price_change_24h}
+                  key={post.price_change_percentage_1h_in_currency}
                   className={`${
-                    post.price_change_24h.toString().startsWith("-")
+                    post.price_change_percentage_1h_in_currency
+                      .toString()
+                      .startsWith("-")
                       ? "text-red-500"
                       : "text-green-500"
                   }`}
                 >
-                  {post.price_change_24h}
+                  <span style={{ display: "flex" }}>
+                    {arrowUpOrDown(post.price_change_percentage_1h_in_currency)}
+                    {formatPercentage(
+                      post.price_change_percentage_1h_in_currency
+                    )}
+                  </span>
+                </TableCell>
+                <TableCell
+                  key={post.price_change_percentage_24h_in_currency}
+                  className={`${
+                    post.price_change_percentage_24h_in_currency
+                      .toString()
+                      .startsWith("-")
+                      ? "text-red-500"
+                      : "text-green-500"
+                  }`}
+                >
+                  <span style={{ display: "flex" }}>
+                    {arrowUpOrDown(
+                      post.price_change_percentage_24h_in_currency
+                    )}
+                    {formatPercentage(
+                      post.price_change_percentage_24h_in_currency
+                    )}
+                  </span>
+                </TableCell>
+                <TableCell
+                  key={post.price_change_percentage_7d_in_currency}
+                  className={`${
+                    post.price_change_percentage_7d_in_currency
+                      .toString()
+                      .startsWith("-")
+                      ? "text-red-500"
+                      : "text-green-500"
+                  }`}
+                >
+                  <span style={{ display: "flex" }}>
+                    {arrowUpOrDown(post.price_change_percentage_7d_in_currency)}
+                    {formatPercentage(
+                      post.price_change_percentage_7d_in_currency
+                    )}
+                  </span>
                 </TableCell>
                 <TableCell key={post.ath}>${post.ath}</TableCell>
               </TableRow>
