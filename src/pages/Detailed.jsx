46312531fb6fb8c9866164210858cf2800151cd0
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 <picard.boris@gmail.com>
2024-03-25 15:13:43 +0100

    formattage timestam pet affichage sur une journée des données d'une crypto et le prix

--- src/pages/Detailed.jsx
+++ src/pages/Detailed.jsx
@@ -10,30 +10,41 @@ import {
 } from "@/components/ui/card";
 import { useCryptoStore } from "@/zustand/store";
 import LineChart from "@/components/LineChart";
+import { useState } from "react";
 
 export default function Detailed() {
   const { cryptos } = useCryptoStore();
   const { id } = useParams();
+  const options = {
+    method: "GET",
+    headers: { x_cg_demo_api_key: "=CG-1t8kdBZJMA1YUmpjF5nypF6R" },
+  };
   const { loading } = FetchCrypto(
-    `https://api.coingecko.com/api/v3/coins/${id}?x_cg_demo_api_key=CG-1t8kdBZJMA1YUmpjF5nypF6R`
+    `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=1, ${options}`
   );
   console.log(cryptos);
   if (loading) {
     return <p>Loading...</p>;
   }
-  const data = [
-    { time: "Janvier", value: 100 },
-    { time: "Février", value: 120 },
-    { time: "Mars", value: 130 },
-    { time: "Avril", value: 110 },
-    { time: "Mai", value: 110 },
-  ];
+
+  const formattedPrices = cryptos.prices.map((entry) => ({
+    time: new Date(entry[0]).toLocaleTimeString(),
+    value: entry[1],
+  }));
+
+  // const data = [
+  //   { time: "Janvier", value: 100 },
+  //   { time: "Février", value: 120 },
+  //   { time: "Mars", value: 130 },
+  //   { time: "Avril", value: 110 },
+  //   { time: "Mai", value: 110 },
+  // ];
 
   return (
     <>
       <div className="container py-10">
-        <div className="flex flex-1 bg-green-300 h-1/2">
-          <LineChart data={data} />
+        <div className="flex justify-center h-1/2">
+          <LineChart data={formattedPrices} />
         </div>
       </div>
     </>
