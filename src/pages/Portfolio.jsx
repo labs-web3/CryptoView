33da121f46846e1696e6d1a0335680f945aede9c
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 <picard.boris@gmail.com>
2024-03-28 11:52:05 +0100

    ajout web3 et connection wallet avec provider infura fonctionnel

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -7,10 +7,46 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
+import { Web3 } from "web3";
+import { useState } from "react";
 export default function MyAccount() {
+  const [connectedAccount, setConnectedAccount] = useState();
+  const [balance, setBalance] = useState();
+
+  const web3 = new Web3(
+    "https://mainnet.infura.io/v3/384d912916a74323b0d3d9583a77b8b9"
+  );
+
+  const connectMetamask = async () => {
+    try {
+      if (window.ethereum) {
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        setConnectedAccount(accounts[0]);
+        getBalance(accounts[0]);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getBalance = async (account) => {
+    try {
+      const balanceWallet = await web3.eth.getBalance(account);
+      setBalance(web3.utils.fromWei(balanceWallet, "ether"));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <div className="container py-16">
+        <div className="flex flex-col mb-4 items-start">
+          <Button onClick={() => connectMetamask()}>Connect my Wallet</Button>
+          <span className="mt-4">{balance}</span>
+        </div>
         <Card className="bg-[#eeeeee] shadow-xl">
           <CardHeader className="pb-3">
             <CardDescription className="text-xl t text-[#777]">
