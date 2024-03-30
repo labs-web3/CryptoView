20a51372700e22deac24cf087afd59f5471299e4
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 <picard.boris@gmail.com>
2024-03-30 11:50:37 +0100

    ajout focus-within et bordure sur div avec input

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -57,16 +57,6 @@ export default function MyAccount() {
   return (
     <>
       <div className="container py-16">
-        {connectedAccount ? (
-          ""
-        ) : (
-          <Button
-            onClick={connectMetamask}
-            className="w-full mt-3 py-8 font-bold text-lg"
-          >
-            Connecter MetaMask
-          </Button>
-        )}
         <div className="flex justify-between text-center">
           <Card className="bg-[#eeeeee] shadow-xl w-1/3">
             <CardHeader className="pb-3">
@@ -97,21 +87,22 @@ export default function MyAccount() {
             </CardContent>
           </Card>
         </div>
-        <div className="bg-black rounded-2xl flex-col justify-center h-1/3 w-1/2 mt-16 flex p-3 gap-3">
-          <div className="flex bg-[#1B1B1B] py-10 rounded-lg justify-between p-3">
+        <div className="bg-black rounded-2xl flex-col justify-center max-h-1/3 w-1/2 mt-16 flex p-3 gap-3">
+          <div className="flex bg-[#1B1B1B] py-10 rounded-lg justify-between p-3 focus-within:border-white border border-transparent">
             <input
               type="number"
               name=""
               id=""
               placeholder="0"
-              className="rounded-xl w-full mr-3 max-h-[44px] text-3xl outline-none text-white font-semibold bg-[#1B1B1B] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+              className="rounded-xl w-full mr-3 max-h-[44px] text-3xl focus:outline-none text-white font-semibold bg-[#1B1B1B] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
             />
             <Button className="rounded-full bg-black hover:bg-neutral-800 font-bold">
               ETH
               <ChevronDown />
             </Button>
           </div>
-          <div className="flex bg-[#1B1B1B] py-10 rounded-lg p-3">
+
+          <div className="flex bg-[#1B1B1B] py-10 rounded-lg p-3 focus-within:border-white border border-transparent">
             <input
               type="number"
               name=""
@@ -124,6 +115,16 @@ export default function MyAccount() {
               <ChevronDown />
             </Button>
           </div>
+          {connectedAccount ? (
+            ""
+          ) : (
+            <Button
+              onClick={connectMetamask}
+              className="w-full py-8 font-bold text-lg bg-[#311C31] hover:bg-[#432643] text-[#FC72FF]"
+            >
+              Connecter MetaMask
+            </Button>
+          )}
         </div>
       </div>
     </>
