d2c5e5ebb327230fa3efee9a1e320d5f5e3186fd
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 <picard.boris@gmail.com>
2024-04-22 13:13:29 +0200

    crud http express done et correction fichier workouts.js

--- server/server.js
+++ server/server.js
@@ -2,21 +2,22 @@
 import express from "express";
 import dotenv from "dotenv";
 import process from "process";
+import workoutRoutes from "./routes/workouts.js";
 
 dotenv.config();
 
 const app = express();
 
 // middleware
+app.use(express.json());
+
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
 // routes
-app.get("/api", (req, res) => {
-  res.json({ message: "Welcome to the app" });
-});
+app.use("/api/workouts/", workoutRoutes);
 
 // listen requests
 app.listen(process.env.PORT, () => {
