63f0b9349babf5e171bbf483aa4a006108cb89ef
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 <picard.boris@gmail.com>
2024-04-30 11:40:31 +0200

    routes protected with a middleware requireAuth verified jwt token

---
+++ server/middleware/requireAuth.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+import process from "process";
+import userModel from "../models/userModel.js";
+
+const requireAuth = async (req, res, next) => {
+  // verify authentication
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    return res.status(401).json({ message: "authorization token required" });
+  }
+
+  const token = authorization.split(" ")[1];
+
+  try {
+    const { _id } = jwt.verify(token, process.env.SECRET);
+
+    req.user = await userModel.findOne({ _id }).select("_id");
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({ message: "Request is not authorized" });
+  }
+};
+
+export default requireAuth;
