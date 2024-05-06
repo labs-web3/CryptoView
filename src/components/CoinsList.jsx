c9d2ee0e8773851e09305a25434f09c1b3c336d1
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 <picard.boris@gmail.com>
2024-05-06 15:31:56 +0200

    new comonents categories and coinlist

--- src/components/CoinsList.jsx
+++ src/components/CoinsList.jsx
@@ -36,7 +36,7 @@ const PriceChangeCell = ({ value }) => {
   );
 };
 
-export default function Categories({ post }) {
+export default function CoinsList({ post }) {
   return (
     <TableRow>
       <Link to={`/${post.id}`}>
@@ -46,17 +46,16 @@ export default function Categories({ post }) {
           } flex items-center`}
         >
           <span className="mr-2">{post.market_cap_rank}.</span>
+          <img
+            width={25}
+            alt={post.name}
+            src={post.image}
+            style={{ marginRight: "5px" }}
+          />
           <span>{post.name}</span>
         </TableCell>
       </Link>
-      <TableCell>
-        <img
-          width={25}
-          alt={post.name}
-          src={post.top_3_coins}
-          style={{ marginRight: "5px" }}
-        />
-      </TableCell>
+      <TableCell>${post.current_price}</TableCell>
       <PriceChangeCell value={post.price_change_percentage_1h_in_currency} />
       <PriceChangeCell value={post.price_change_percentage_24h_in_currency} />
       <PriceChangeCell value={post.price_change_percentage_7d_in_currency} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,6 +13,7 @@ import { useEffect } from "react";
 import Pagination from "@/components/Pagination";
 import { Button } from "@/components/ui/button";
 import Categories from "@/components/Categories";
+import CoinsList from "@/components/CoinsList";
 
 export default function Home() {
   const { pageNumber = 1 } = useParams();
@@ -55,15 +56,6 @@ export default function Home() {
 
   const handleCategories = () => {
     navigate("/categories");
-  };
-
-  const formatPercentage = (value) => {
-    return Intl.NumberFormat("en-US", {
-      style: "percent",
-      minimumFractionDigits: 1,
-      maximumFractionDigits: 1,
-      signDisplay: "never",
-    }).format(value / 100);
   };
 
   const arrowUpOrDown = (value) => {
@@ -149,91 +141,9 @@ export default function Home() {
             </>
           ) : (
             <>
-              {top.data.map((post, index) => (
-                <TableRow key={index}>
-                  <Link to={`/${post.id}`}>
-                    <TableCell
-                      key={post.name}
-                      className={`${
-                        post.name === "Chiliz" ? "text-red-500" : ""
-                      }`}
-                      style={{ display: "flex", alignItems: "center" }}
-                    >
-                      <span style={{ marginRight: "5px" }}>
-                        {post.market_cap_rank}.
-                      </span>
-                      <img
-                        width={25}
-                        alt={post.name}
-                        src={post.image}
-                        style={{ marginRight: "5px" }}
-                      />
-                      <span>{post.name}</span>
-                    </TableCell>
-                  </Link>
-                  <TableCell key={post.current_price}>
-                    ${post.current_price}
-                  </TableCell>
-                  <TableCell
-                    key={post.price_change_percentage_1h_in_currency}
-                    className={`${
-                      post.price_change_percentage_1h_in_currency
-                        ?.toString()
-                        .startsWith("-")
-                        ? "text-red-500"
-                        : "text-green-500"
-                    }`}
-                  >
-                    <span style={{ display: "flex" }}>
-                      {arrowUpOrDown(
-                        post.price_change_percentage_1h_in_currency
-                      )}
-                      {formatPercentage(
-                        post.price_change_percentage_1h_in_currency
-                      )}
-                    </span>
-                  </TableCell>
-                  <TableCell
-                    key={post.price_change_percentage_24h_in_currency}
-                    className={`${
-                      post.price_change_percentage_24h_in_currency
-                        ?.toString()
-                        .startsWith("-")
-                        ? "text-red-500"
-                        : "text-green-500"
-                    }`}
-                  >
-                    <span style={{ display: "flex" }}>
-                      {arrowUpOrDown(
-                        post.price_change_percentage_24h_in_currency
-                      )}
-                      {formatPercentage(
-                        post.price_change_percentage_24h_in_currency
-                      )}
-                    </span>
-                  </TableCell>
-                  <TableCell
-                    key={post.price_change_percentage_7d_in_currency}
-                    className={`${
-                      post.price_change_percentage_7d_in_currency
-                        ?.toString()
-                        .startsWith("-")
-                        ? "text-red-500"
-                        : "text-green-500"
-                    }`}
-                  >
-                    <span style={{ display: "flex" }}>
-                      {arrowUpOrDown(
-                        post.price_change_percentage_7d_in_currency
-                      )}
-                      {formatPercentage(
-                        post.price_change_percentage_7d_in_currency
-                      )}
-                    </span>
-                  </TableCell>
-                  <TableCell key={post.ath}>${post.ath}</TableCell>
-                </TableRow>
-              ))}
+              {top.data.map((post, index) => {
+                return <CoinsList post={post} key={index} />;
+              })}
             </>
           )}
         </TableBody>
