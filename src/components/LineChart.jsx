a3eb1956ce2c7a84c0666dece050cde0ebbc4c32
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 <picard.boris@gmail.com>
2024-05-06 18:22:00 +0200

    ajout d'une ligne vertical sur la chart

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,34 +1,43 @@
 import { Line } from "react-chartjs-2";
-import {
-  Chart as ChartJS,
-  LineElement,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  Tooltip,
-} from "chart.js";
+import { Chart as ChartJS, registerables } from "chart.js";
 
-// Assurez-vous de bien enregistrer Tooltip également
-ChartJS.register(
-  LineElement,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  Tooltip
-);
+ChartJS.register(...registerables);
+
+// Création d'un plugin pour dessiner une ligne verticale
+const verticalLinePlugin = {
+  id: "verticalLineOnTooltip",
+  afterDraw: (chart) => {
+    if (chart.tooltip.getActiveElements().length > 0) {
+      const ctx = chart.ctx;
+      const tooltipElement = chart.tooltip.getActiveElements()[0];
+      const x = tooltipElement.element.x;
+      const yAxis = chart.scales.y;
+
+      ctx.save();
+      ctx.beginPath();
+      ctx.moveTo(x, yAxis.top);
+      ctx.lineTo(x, yAxis.bottom);
+      ctx.lineWidth = 1;
+      ctx.strokeStyle = "rgba(0, 0, 255, 0.4)";
+      ctx.stroke();
+      ctx.restore();
+    }
+  },
+};
+
+ChartJS.register(verticalLinePlugin);
 
 export default function LineChart({ data }) {
   const hourlyLabels = data.map((entry) => entry.time);
-
   const chartData = {
     labels: hourlyLabels,
     datasets: [
       {
         label: "Valeur Crypto",
-        data: data.map((entry) => entry.value),
-        fill: false,
+        data: data.map((entry) => entry.value.toFixed(5)),
         borderColor: "rgba(75,192,192,1)",
         tension: 0.1,
+        fill: false,
       },
     ],
   };
@@ -55,9 +64,5 @@ export default function LineChart({ data }) {
     },
   };
 
-  return (
-    <>
-      <Line data={chartData} options={chartOptions} />
-    </>
-  );
+  return <Line data={chartData} options={chartOptions} />;
 }
